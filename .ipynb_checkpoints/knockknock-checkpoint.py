603b4c6b6810f06f8954084a5e4be648aa0b1b6f
zhongfei <ongzhongfei@gmail.com>
2020-11-21 19:30:51 +0800

    update text

--- .ipynb_checkpoints/knockknock-checkpoint.py
+++ .ipynb_checkpoints/knockknock-checkpoint.py
@@ -61,7 +61,7 @@ if col1.checkbox("Sounds like someone is knocking on the door..."):
                 file_.close()
 
                 col4.write(
-                    f'<img src="data:image/gif;base64,{data_url}" style="float:left">',
+                    f'<img src="data:image/gif;base64,{data_url}" style="float:right">',
                     unsafe_allow_html=True,
                 )
 
@@ -71,10 +71,9 @@ if col1.checkbox("Sounds like someone is knocking on the door..."):
                         f"""<img src="https://media1.tenor.com/images/7f030fc5351142d303f081db14b914ca/tenor.gif?itemid=10086712" style="float:left">""",
                         unsafe_allow_html=True,
                     )
-                    # st.write( "<p style='text-align: left;'>(This is me. Cutie)</p>",    unsafe_allow_html=True)  
                     if col6.checkbox("???"):
                         col6.write(
-                            f"""<img src="https://toppng.com/uploads/preview/confused-anime-png-anime-question-png-gif-11563638774vp3mpnwbez.png" style="float:left" width="240" height="240">""",
+                            f"""<img src="https://toppng.com/uploads/preview/confused-anime-png-anime-question-png-gif-11563638774vp3mpnwbez.png" style="float:right" width="240" height="240">""",
                             unsafe_allow_html=True,
                         )
 
@@ -84,10 +83,9 @@ if col1.checkbox("Sounds like someone is knocking on the door..."):
                                 f"""<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7ir_TizdZ7vXZS8V98g2Lll6YREYYqdYt7A&usqp=CAU" style="float:left">""",
                                 unsafe_allow_html=True,
                             )
-                            # st.write( "<p style='text-align: left;'>(This is me. Cutie)</p>",    unsafe_allow_html=True)  
                             if col8.checkbox("..."):
                                 col8.write(
-                                    f"""<img src="https://qph.fs.quoracdn.net/main-qimg-6ed14162acb25116839c2bc2d601e4e3" style="float:left" width="360" height="250" >""",
+                                    f"""<img src="https://qph.fs.quoracdn.net/main-qimg-6ed14162acb25116839c2bc2d601e4e3" style="float:right" width="360" height="250" >""",
                                     unsafe_allow_html=True,
                                 )
                                 col9, col10 = st.beta_columns(2)
@@ -99,7 +97,7 @@ if col1.checkbox("Sounds like someone is knocking on the door..."):
                                     # st.write( "<p style='text-align: left;'>(This is me. Cutie)</p>",    unsafe_allow_html=True)  
                                     if col10.checkbox("Niceee. Good boy."):
                                         col10.write(
-                                            f"""<img src="https://i.pinimg.com/736x/3b/20/33/3b2033cd7657842acb28440b90ade3ee.jpg" style="float:left" width="360" height="250" >""",
+                                            f"""<img src="https://i.pinimg.com/736x/3b/20/33/3b2033cd7657842acb28440b90ade3ee.jpg" style="float:right" width="360" height="250" >""",
                                             unsafe_allow_html=True,
                                         )
 
